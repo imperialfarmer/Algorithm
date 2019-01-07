2ee713042fd7b27d0dad0450bcafbef6072c2df0
imperialfarmer <yin062398@gmail.com>
2019-01-07 00:40:59 +0000

    Update dijkstra.py

--- graph/dijkstra.py
+++ graph/dijkstra.py
@@ -1,18 +1,16 @@
 # -*- using: utf-8 -*-
 """
 dijkstra algorithm for Path-finding
+Solution: 2599,2610,3038,2052,2367,2399,2029,2442,3386,3578
+          2599, 2610, 2947, 2052, 2367, 2399, 2029, 2442, 2505, 3068
 """
 
 import sys
-import heapq
+from heapq import *
 
 
 #! prepare the data
 global nodeNum
-global track
-global visited
-global distQueue
-distQueue = []
 
 
 def readDirectedEdge(fileName):
@@ -39,26 +37,62 @@ def readDirectedEdge(fileName):
 
 
 #! dijkstra algorithm
-def dijkstra(graph, sourceVertex):
-    #! mark the node visited
-    visited.add(sourceVertex)
-    #! calculate the distances from source vertex
-    
-    for adjNode in graph[sourceVertex][0]:
-        if adjNode not in visited:
-            dijkstra(graph,adjNode)
+def dijkstra(graph, s, t):
+    """
+    s: source vertex
+    t: target vertex
+    """
+    # initialize containers
+    distQueue = [(0,s,())]
+    visited = set()
+    track = {s:0}
 
+    while distQueue:
+        # get the path with minimum distance so far
+        dist_v, v, path = heappop(distQueue)
+        if v not in visited:
+            visited.add(v)  # mark the node visited
+            path = path + (v,) # add v into path
+            
+            # if the searching reaches the target
+            if v == t:
+                return (dist_v, path)
+            
+            # if not, continue searching the neighboured vertex
+            for i in range(len(graph[v][0])):
+                w = graph[v][0][i]
+                if w not in visited:
+                    dist_vw = graph[v][1][i]
+                    dist_w = dist_v + dist_vw
 
-
-
+                    # read the previous distance record from 'track'
+                    # will get None if there is no record
+                    prevDist_vw = track.get(w, None)
+                    # update the recorded distance if the current path is better
+                    if prevDist_vw is None or dist_w < prevDist_vw:
+                        track[w] = dist_vw
+                        heappush(distQueue, (dist_w, w, path))
 
 
 if __name__ == '__main__':
+    print('\n-> Input Data')
     graph = readDirectedEdge('./dijkstraData.txt')
+    # print(graph)
 
-    track = [1000000]*nodeNum
-    track[0] = 0
-    visited = set()
+    print('-> Searching Shortest Path')
+    targets = [7,37,59,82,99,115,133,165,188,197]
+    solution = []
+    for t in targets:
+        dist, path = dijkstra(graph,1,t)
+        print('-- TO '+ str(t) + ' --')
+        print(dist, path)
+        print('\n')
+        solution.append(dist)
+
+    print('Final solution')
+    print(solution)
+    
+
 
 
 
